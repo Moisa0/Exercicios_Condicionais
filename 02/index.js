5b2e9268c60a0cb0e0ea0dc185a7643bfb26a850
Moisa0 <95927203+Moisa0@users.noreply.github.com>
2023-08-21 21:13:38 -0300

    Resolução 02 da atividade

--- 02/index.js
+++ 02/index.js
@@ -1,4 +1,8 @@
-const jogada1 = 5;
+const jogada1 = 2;
 const jogada2 = 3;
 
-//seu código aqui
+if((jogada1 + jogada2) % 2 === 0){
+    console.log("Par ganhou!")
+} else{
+    console.log("Ímpar ganhou!")
+}
